4bca05336c1fd0f9032b1192c9b1f9e6d59e4453
pierreasd <pierreasd@gmail.com>
2021-02-26 17:46:08 +0900

    Updates

---
+++ precache-manifest.2d8b244e361b7534d26715d05e7e3db2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930153787c4e173d67e30ceaa082e22e",
+    "url": "/ptisbcoid/index.html"
+  },
+  {
+    "revision": "b09e67b99c07e4169cb6",
+    "url": "/ptisbcoid/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "acf97171ca396cdedffb",
+    "url": "/ptisbcoid/static/js/2.026a37c3.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/ptisbcoid/static/js/2.026a37c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09e67b99c07e4169cb6",
+    "url": "/ptisbcoid/static/js/main.7dd427d0.chunk.js"
+  },
+  {
+    "revision": "df0fd0ab7ae477daa723",
+    "url": "/ptisbcoid/static/js/runtime-main.a6da08f1.js"
+  },
+  {
+    "revision": "6317a64397fc0ce3068edc8fd9bb0289",
+    "url": "/ptisbcoid/static/media/gold.6317a643.png"
+  },
+  {
+    "revision": "575da6d5c621edd52c73258d3628a2be",
+    "url": "/ptisbcoid/static/media/proyek.575da6d5.jpg"
+  },
+  {
+    "revision": "5c53e21e609857551246e7f92a118a71",
+    "url": "/ptisbcoid/static/media/rock.5c53e21e.jpg"
+  },
+  {
+    "revision": "6550957d9aab132566ba82825d01a893",
+    "url": "/ptisbcoid/static/media/taliwang.6550957d.jpg"
+  },
+  {
+    "revision": "5dab922a4c21e6b9a3f7a0271e6dbc6d",
+    "url": "/ptisbcoid/static/media/temp.5dab922a.jpg"
+  }
+]);
